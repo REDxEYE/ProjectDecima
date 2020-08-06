86e4e4e753533c07c5da7f2ca1abade3629f9abd
RED_EYE <med45c@gmail.com>
2020-08-06 11:31:06 +0300

    removed LGLE

--- ProjectDS.cmake
+++ ProjectDS.cmake
@@ -1,5 +1,5 @@
-add_executable(ProjectDS
-        src/main.cpp
+add_executable(NoLGLEProjectDS
+        src/no_lgle_main.cpp
         src/archive.cpp
         src/file_types/prefetch.cpp
         src/archive_array.cpp
@@ -7,16 +7,18 @@ add_executable(ProjectDS
         src/file_types/prefetch.cpp
         src/archive_tree.cpp
         src/file_types/core.cpp
-        src/archive_file.cpp)
+        src/archive_file.cpp src/imgui_backend_impl.cpp)
 
-target_include_directories(ProjectDS PUBLIC include ${HASHLIB_INC} ${OOZLIB_INC} ${LGLE_INCLUDE} ${LGLE_LIB_INCLUDE})
+target_include_directories(NoLGLEProjectDS PUBLIC include ${HASHLIB_INC} ${OOZLIB_INC})
 
+target_include_directories(NoLGLEProjectDS PUBLIC
+        libs/glad/include
+        libs/glfw/include
+        )
 
-target_link_libraries(ProjectDS PUBLIC
-        HashLib oozLib LGLE
+target_link_libraries(NoLGLEProjectDS PUBLIC
+        HashLib oozLib glfw glad imgui
         -static-libgcc -static-libstdc++ -static
         -Wl,-Bstatic -lstdc++ -lpthread
         -Wl,-Bdynamic
         -fuse-ld=lld)
-
-target_precompile_headers(ProjectDS PUBLIC <tchar.h> libs/LGLE/lib/imgui/imgui.h include/imgui_memory_editor.h include/portable-file-dialogs.h <unordered_map> <vector>)
