de6abd4736a049d625ad4dabde8daa69c76a02bb
ShadelessFox <yanuari.channel@gmail.com>
2020-08-08 15:23:32 +0300

    Disable corrupted texture parsing for now

--- src/decima/file_types/texture.cpp
+++ src/decima/file_types/texture.cpp
@@ -3,14 +3,12 @@
 //
 #include "decima/file_types/texture.h"
 
-void Decima::Texture::parse(std::vector<uint8_t>& buffer) {
-    imemstream imembuffer(buffer);
-    parse(imembuffer);
-}
-void Decima::Texture::parse(std::istream& stream) {
-    CoreFile::parse(stream);
-    uint64_t start = stream.tellg();
-    stream.read(reinterpret_cast<char*>(&unk1), 4 + 4 + 4 + 16 + 4 + 4 + 16);
 
-    stream.seekg(start + header.file_size - 16 /*GUID*/, std::ios::beg);
+void Decima::Texture::parse(Source& stream) {
+//    CoreFile::parse(stream);
+//    uint64_t start = stream.tell();
+//
+//    unk1 = stream.read(reinterpret_cast<char*>(&unk1), 4 + 4 + 4 + 16 + 4 + 4 + 16);
+//
+//    stream.seekg(start + header.file_size - 16 /*GUID*/, std::ios::beg);
 }
