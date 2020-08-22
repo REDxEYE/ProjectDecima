3904e5b55ef1a832cf20d678d9b79e1f7e0c3cbe
ShadelessFox <yanuari.channel@gmail.com>
2020-08-22 19:44:30 +0300

    Add missing collection.hpp

---
+++ include/decima/file_types/core/collection.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "decima/file_types/core/core.h"
+#include "decima/file_types/pod/reference.hpp"
+
+namespace Decima {
+    class Collection : public CoreFile {
+    public:
+        void parse(ArchiveArray& archives, Source& stream) override;
+        void draw() override;
+
+        std::vector<Reference> refs;
+    };
+}
