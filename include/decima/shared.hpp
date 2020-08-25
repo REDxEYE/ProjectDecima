df33ce94d52f98f899da2f85b762f1dfb6981245
ShadelessFox <yanuari.channel@gmail.com>
2020-08-25 22:22:13 +0300

    Add macro for retrieving version

--- include/decima/shared.hpp
+++ include/decima/shared.hpp
@@ -12,9 +12,23 @@ namespace Decima {
 }
 
 #ifdef _MSC_VER
-#define DECIMA_PACK(_Def) __pragma(pack(push, 1)) _Def __pragma(pack(pop))
+    #define DECIMA_PACK(_Def) __pragma(pack(push, 1)) _Def __pragma(pack(pop))
 #else
-#define DECIMA_PACK(_Def) _Def __attribute__((packed))
+    #define DECIMA_PACK(_Def) _Def __attribute__((packed))
 #endif
+
+#define __DECIMA_STR2(s) #s
+#define __DECIMA_STR1(s) __DECIMA_STR2(s)
+
+#define DECIMA_VERSION_MAJOR 0
+#define DECIMA_VERSION_MINOR 1
+#define DECIMA_VERSION_MICRO 5
+
+// clang-format off
+#define DECIMA_VERSION \
+    __DECIMA_STR1(DECIMA_VERSION_MAJOR) "." \
+    __DECIMA_STR1(DECIMA_VERSION_MINOR) "." \
+    __DECIMA_STR1(DECIMA_VERSION_MICRO)
+// clang-format on
 
 #endif //PROJECTDS_SHARED_HPP
